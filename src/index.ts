7a1fbbb1738154b1e9ce139ed75b5b2501e03ff2
dawidseipold <seipold.dawid@gmail.com>
2024-05-10 04:30:38 +0200

    chore: clean up the main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
-import { userTable } from "./db/schema/auth";
 import { Env } from "./context";
+import { usersRoute } from "./routes/users";
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/", async (c) => {
-  const sql = neon(c.env.DATABASE_URL);
-  const db = drizzle(sql);
-
-  const allUsers = await db.select().from(userTable);
-
-  return c.json(allUsers);
-});
+app.basePath("/api").route("/users", usersRoute);
 
 export default app;
